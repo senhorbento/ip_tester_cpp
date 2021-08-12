ce2874669a388764d9f2c871fe7c4babef517734
Henrique Bento <68090144+senhorbento@users.noreply.github.com>
2021-08-11 22:51:30 -0300

    Now with regex!

--- ip_tester.cpp
+++ ip_tester.cpp
@@ -3,64 +3,89 @@
 #include <list>
 #include <cstdio>
 #include <cstring>
+#include <regex>
 
 using namespace std;
 
 bool ligado(string arq){
 	bool aux;
-	string leitura, desligado;
+	string leitura, inativo;
 	list <string> resultado;
 	ifstream input_file(arq);
-	desligado="Host de destino inacess";
+	inativo="Host de destino inacess";
 
     while(getline(input_file, leitura)){
         resultado.push_back(leitura);
     };
 	input_file.close();
 
-	for(auto confere : resultado)
-		if(strstr(confere.c_str(),desligado.c_str()) > 0) 
-			return aux=0;
+	for(auto confere : resultado) if(strstr(confere.c_str(),inativo.c_str()) > 0) return aux=0;
 
 	return aux=1;
 }
 
+string obterIP(string s){
+		string lista[20];
+		char ip[20];
+		smatch m;
+		regex e ("[_]([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]){1,3}(\\.([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]){1,3}){3}");
+		int qtd, i;
+		qtd=0;
+
+		while(regex_search (s,m,e)){
+			for(auto x:m){
+				lista[qtd]=x;
+				qtd++;
+			} 
+			s = m.suffix().str();
+		}
+		strcpy(ip, lista[0].c_str());
+		for(i=0;i<strlen(ip);i++) ip[i]=ip[i+1];
+	
+		return ip;
+
+}
+
 int main(){
+	int qtd;
 	bool removido;
-	string leitura,teste, abrir, entrada, saida;
+	string leitura,teste, abrir, entrada, saida,ip;
 	list <string> lista, ligados;
-	removido=0;entrada = "lista.txt";saida = "resultado.txt";
+	qtd=-1; entrada = "lista.txt"; saida = "resultado.txt";
 	ifstream input_file(entrada);
 	ofstream output_file;
 
-	while(getline(input_file, leitura)){
-        lista.push_back(leitura);
-    };
-	input_file.close();
+	system("cls");
+	while(qtd!=0){
+		qtd=1; removido=0;
+		while(getline(input_file, leitura)){
+			lista.push_back(leitura);
+		};
+		input_file.close();
 
-	for(auto ip : lista){
-		teste="ping -n 1 -i 150 " + ip + " > " + saida;
-		cout << "Pingando para " << ip;
-        system(teste.c_str());
-		if(ligado(saida)){
-			cout << " - Ligado!" << endl;
-			abrir="\"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe\" " + ip;
-			system(abrir.c_str());
-			ligados.push_back(ip);
-			removido=1;
+		for(auto linha : lista){
+			ip=obterIP(linha);
+			teste="ping -n 1 -i 150 " + ip + " > " + saida;
+			system(teste.c_str());
+			if(ligado(saida)){
+				abrir="\"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe\" " + ip;
+				system(abrir.c_str());
+				ligados.push_back(ip);
+				removido=1;
+			}
+			qtd++;
 		}
-		else cout << " - Desligado!" << endl;
-    }
 
-	if (removido){
-		for(auto ip : ligados)
-		lista.remove(ip);
-		ligados.clear();
-		system("del /f lista.txt");
-		output_file.open(entrada);
-		for(auto ip : lista)
-				output_file << ip << endl;
-		output_file.close();
+		if (removido){
+			for(auto ip : ligados) lista.remove(ip);
+			system("del lista.txt && del resultado.txt");
+			output_file.open(entrada);
+			for(auto ip : lista) output_file << ip << endl;
+			output_file.close();
+			ligados.clear();
+		}
+		system("pause");
+		cout << "A lista foi pesquisada " << n << " vezes" << endl;
 	}
 
 	return 0;
