52cabd6fc82efe9eaa3531a25eb9a4e007834876
Henrique Bento <68090144+senhorbento@users.noreply.github.com>
2021-09-08 20:10:51 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <cstdio>
 #include <cstring>
 #include <regex>
+#include <sys/stat.h>
 
 using namespace std;
 
@@ -66,9 +67,14 @@ void LimparTela() {
 
 void Pause(int v) {
 	switch (v) {
-	case 1: cout << endl << "Pressione uma tecla para remover da lista e continuar!" << endl; getchar(); break;
-	case 2: cout << endl << "Pressione uma tecla para finalizar o programa!" << endl; getchar(); break;
+	case 1: cout << endl << "Ip Ativo!" << endl << "Pressione uma tecla para remover da lista e continuar!\n"; getchar(); break;
+	case 2: cout << "Lista Finalizada!" << endl << "Pressione uma tecla para finalizar o programa!\n"; getchar(); break;
 	case 3: cout << endl << "Finalizando o programa devido ao erro!" << endl; getchar(); break;
+	case 4: cout << "Parece que voce esta executando o programa pela primeira vez.\n"
+	             << "Acesse a pasta txt e siga as instrucoes dentro de cada arquivo =\)\n\n"
+				 << "Pressione uma tecla para finalizar o programa e abrir a pasta!\n"; getchar(); break;
+	case 5: cout << "Nao foi encontrado o arquivo ignore.txt\n"
+				 << "Criando o arquivo ignore.txt...\n\n" << "Pressione uma tecla para continuar a execucao\n"; getchar(); break;
 	}
 }
 
@@ -90,6 +96,30 @@ void Abrir(string ip) {
 	system(abrir.c_str());
 }
 
+
+int ExisteArquivo(const char* filename){
+    struct stat buffer;
+    int exist = stat(filename, &buffer);
+    return exist;
+}
+
+void CriarArquivo(string txt){
+	string texto;
+	if(txt == _ENTRADA_){
+		texto = "echo \/\/Remova essas linhas e coloque os enderecos a serem testados aqui =\) > " + txt;
+		system(texto.c_str());
+		texto = "echo \/\/Remove those lines and put the addresses to be tested here =\) >> " + txt;
+		system(texto.c_str());
+	}
+	else if(txt == _IGNORE_){
+		texto = "echo \/\/Remova essas linhas e coloque os enderecos a serem ignorados aqui =\( > " + txt;
+		system(texto.c_str());
+		texto = "echo \/\/Remove those lines and put the addresses to be ignored here =\( >> " + txt;
+		system(texto.c_str());
+	}
+
+}
+
 int main() {
 	int qtd, n;
 	bool removido;
@@ -98,15 +128,32 @@ int main() {
 	ifstream input_file;
 	ofstream output_file;
 
-	input_file.open(_IGNORE_);
-	if (input_file.is_open()) {
-		while (!input_file.eof()) {
-			getline(input_file, leitura);
-			if (leitura != "\0") {
-				ignore.push_back(leitura);
+	if(ExisteArquivo(_ENTRADA_) == -1 && ExisteArquivo(_IGNORE_) == -1){
+		system("mkdir txt");
+		CriarArquivo(_ENTRADA_);
+		CriarArquivo(_IGNORE_);
+		LimparTela(); 
+		Pause(4);
+		system("explorer.exe /e, /n, txt");
+		return EXIT_FAILURE;
+	}
+
+	if(!ExisteArquivo(_IGNORE_)){
+		input_file.open(_IGNORE_);
+		if (input_file.is_open()) {
+			while (!input_file.eof()) {
+				getline(input_file, leitura);
+				if (leitura != "\0") {
+					ignore.push_back(leitura);
+				}
 			}
+			input_file.close();
 		}
-		input_file.close();
+	}
+	else {
+		LimparTela();
+		CriarArquivo(_IGNORE_);
+		Pause(5);
 	}
 
 	LimparTela();
@@ -129,12 +176,12 @@ int main() {
 			n++;
 			ip = obterIP(linha, ignore);
 			if (ip == "ERROR") {
-				cout << "Foi encontrado um erro no elemento de posicao " << n << ", favor verificar a lista =)" << endl << endl;
+				cout << "Nao foram encontrados ips na linha " << n << ", favor verificar a lista =)\n";
 				qtd = -1;
 				break;
 			}
 			else {
-				cout << n << ". Testando o ip " << ip << endl;
+				cout << "Testando o ip " << ip << endl << n << "/" << qtd << endl;
 				Pingar(ip, _SAIDA_);
 				if (ligado(_SAIDA_)) {
 					Abrir(ip);
@@ -160,12 +207,12 @@ int main() {
 		}
 		lista.clear();
 	} while (qtd > 0);
+
 	if (qtd == -1)
 		Pause(3);
 	else {
 		LimparTela();
-		Remover(_ENTRADA_);
-		cout << "Fim de lista!" << endl;
+		CriarArquivo(_ENTRADA_);
 		Pause(2);
 	}
 	return 0;
